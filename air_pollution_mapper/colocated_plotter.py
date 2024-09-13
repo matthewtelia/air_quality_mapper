b3cca2edecad73cc5051bf527bc4c7173d00dd15
Matthew Elia <matthewtelia95@gmail.com>
2024-09-13 15:47:57 -0400

    add collated plotter and data_plotter

--- air_pollution_mapper/colocated_plotter.py
+++ air_pollution_mapper/colocated_plotter.py
@@ -55,10 +55,10 @@ google_pm25_247_data = process_google_data(google_247_data, google247_name)
 # Plot the data
 plot_data(chelsea_epa_data,mod_pm25_247_data,google_pm25_247_data,chelsea_epa_name,mod247_name,google247_name)
 
-epa_mod_247_data = merge_data(chelsea_epa_data,mod_pm25_247_data,chelsea_epa_name,mod247_name)
+merged_247_data,merged_247_entries = merge_data(chelsea_epa_data,mod_pm25_247_data,google_pm25_247_data,chelsea_epa_name,mod247_name,google247_name)
 #calculate error
-plot_regression_plot(epa_mod_247_data,chelsea_epa_name,mod247_name)
-error_247 = calculate_error(epa_mod_247_data, chelsea_epa_name, mod247_name)
+plot_regression_plot(merged_247_data,chelsea_epa_name,mod247_name,google247_name)
+error_247,mae_247_quantaq, rmse_247_quantaq, re_247_quantaq, mae_247_google, rmse_247_google,re_247_google = calculate_error(merged_247_data, chelsea_epa_name, mod247_name,google247_name)
 
 #############################################
 #Mod 238
@@ -70,10 +70,10 @@ mod_pm25_238_data,mod_238_hourly= process_mod_data(mod_238_data,mod_238_raw_data
 google_pm25_238_data = process_google_data(google_238_data, google238_name)
 # Plot the data
 plot_data(vh_epa_data,mod_pm25_238_data,google_pm25_238_data,vh_epa_name,mod238_name,google238_name)
-epa_mod_238_data = merge_data(vh_epa_data,mod_pm25_238_data,vh_epa_name,mod238_name)
+merged_238_data,merged_238_entries = merge_data(vh_epa_data,mod_pm25_238_data,google_pm25_238_data,vh_epa_name,mod238_name,google238_name)
 #calculate error
-plot_regression_plot(epa_mod_238_data,vh_epa_name,mod238_name)
-error_238 = calculate_error(epa_mod_238_data, vh_epa_name, mod238_name)
+plot_regression_plot(merged_238_data,vh_epa_name,mod238_name,google238_name)
+error_238,mae_238_quantaq, rmse_238_quantaq, re_238_quantaq, mae_238_google, rmse_238_google,re_238_google = calculate_error(merged_238_data, vh_epa_name, mod238_name,google238_name)
 
 
 #############################################
@@ -87,11 +87,11 @@ google_pm25_248_data = process_google_data(google_248_data, google248_name)
 
 # Plot the data
 plot_data(vh_epa_data,mod_pm25_248_data,google_pm25_248_data,vh_epa_name,mod248_name,google248_name)
-epa_mod_248_data = merge_data(vh_epa_data,mod_pm25_248_data,vh_epa_name,mod248_name)
+merged_248_data,merged_248_entries = merge_data(vh_epa_data,mod_pm25_248_data,google_pm25_248_data,vh_epa_name,mod248_name,google248_name)
 #calculate error
-plot_regression_plot(epa_mod_248_data,vh_epa_name,mod248_name)
+plot_regression_plot(merged_248_data,vh_epa_name,mod248_name,google248_name)
 #calculate error
-error_248 = calculate_error(epa_mod_248_data, vh_epa_name, mod248_name)
+error_248,mae_248_quantaq, rmse_248_quantaq, re_248_quantaq, mae_248_google, rmse_248_google,re_248_google = calculate_error(merged_248_data, vh_epa_name, mod248_name,google248_name)
 
 #############################################
 #Mod 246
@@ -105,11 +105,11 @@ google_pm25_246_data = process_google_data(google_246_data, google246_name)
 
 # Plot the data
 plot_data(rox_epa_data,mod_pm25_246_data,google_pm25_246_data,rox_epa_name,mod246_name,google246_name)
-epa_mod_246_data = merge_data(rox_epa_data,mod_pm25_246_data,rox_epa_name,mod246_name)
+merged_246_data, merged_246_entries = merge_data(rox_epa_data,mod_pm25_246_data,google_pm25_246_data,rox_epa_name,mod246_name,google246_name)
 #calculate error
-plot_regression_plot(epa_mod_246_data,rox_epa_name,mod246_name)
+plot_regression_plot(merged_246_data,rox_epa_name,mod246_name,google246_name)
 #calculate error
-error_246 = calculate_error(epa_mod_246_data, rox_epa_name, mod246_name)
+error_246,mae_246_quantaq, rmse_246_quantaq, re_246_quantaq, mae_246_google, rmse_246_google,re_246_google = calculate_error(merged_246_data, rox_epa_name, mod246_name,google246_name)
 
 #############################################
 #Mod 257
@@ -123,11 +123,11 @@ google_pm25_257_data = process_google_data(google_257_data, google257_name)
 
 # Plot the data
 plot_data(rox_epa_data,mod_pm25_257_data,google_pm25_257_data,rox_epa_name,mod257_name,google257_name)
-epa_mod_257_data = merge_data(rox_epa_data,mod_pm25_257_data,rox_epa_name,mod257_name)
+merged_257_data,merged_257_entries = merge_data(rox_epa_data,mod_pm25_257_data,google_pm25_257_data,rox_epa_name,mod257_name,google257_name)
 #calculate error
-plot_regression_plot(epa_mod_257_data,rox_epa_name,mod257_name)
+plot_regression_plot(merged_257_data,rox_epa_name,mod257_name,google257_name)
 #calculate error
-error_257 = calculate_error(epa_mod_257_data, rox_epa_name, mod257_name)
+error_257,mae_257_quantaq, rmse_257_quantaq, re_257_quantaq, mae_257_google, rmse_257_google,re_257_google = calculate_error(merged_257_data, rox_epa_name, mod257_name,google257_name)
 
 #############################################
 #Mod 255
@@ -139,11 +139,11 @@ mod_pm25_255_data,mod_255_hourly= process_mod_data(mod_255_data,mod_255_raw_data
 google_pm25_255_data = process_google_data(google_255_data, google255_name)
 # Plot the data
 plot_data(vh_epa_data,mod_pm25_255_data,google_pm25_255_data,vh_epa_name,mod255_name,google255_name)
-epa_mod_255_data = merge_data(vh_epa_data,mod_pm25_255_data,vh_epa_name,mod255_name)
+merged_255_data,merged_255_entries = merge_data(vh_epa_data,mod_pm25_255_data,google_pm25_255_data,vh_epa_name,mod255_name,google255_name)
 #calculate error
-plot_regression_plot(epa_mod_255_data,vh_epa_name,mod255_name)
+plot_regression_plot(merged_255_data,vh_epa_name,mod255_name,google255_name)
 #calculate error
-error_255 = calculate_error(epa_mod_255_data, vh_epa_name, mod255_name)
+error_255,mae_255_quantaq, rmse_255_quantaq, re_255_quantaq, mae_255_google, rmse_255_google,re_255_google = calculate_error(merged_255_data, vh_epa_name, mod255_name,google255_name)
 
 #############################################
 #Mod 256
@@ -155,9 +155,103 @@ mod_pm25_256_data,mod_256_hourly= process_mod_data(mod_256_data,mod_256_raw_data
 google_pm25_256_data = process_google_data(google_256_data, google256_name)
 # Plot the data
 plot_data(rox_epa_data,mod_pm25_256_data,google_pm25_256_data,rox_epa_name,mod256_name,google256_name)
-epa_mod_256_data = merge_data(rox_epa_data,mod_pm25_256_data,rox_epa_name,mod256_name)
+merged_256_data,merged_256_entries = merge_data(rox_epa_data,mod_pm25_256_data,google_pm25_256_data,rox_epa_name,mod256_name,google256_name)
 #calculate error
-plot_regression_plot(epa_mod_256_data,rox_epa_name,mod256_name)
+plot_regression_plot(merged_256_data,rox_epa_name,mod256_name,google256_name)
 #calculate error
-error_256 = calculate_error(epa_mod_256_data, rox_epa_name, mod256_name)
+error_256,mae_256_quantaq, rmse_256_quantaq, re_256_quantaq, mae_256_google, rmse_256_google,re_256_google = calculate_error(merged_256_data, rox_epa_name, mod256_name,google256_name)
 
+
+ # %%
+import matplotlib.pyplot as plt
+
+# Calculate the number of entries for each data set
+num_entries = [merged_247_entries, merged_238_entries, merged_248_entries,  merged_255_entries, merged_246_entries, merged_256_entries, merged_257_entries]
+data_labels = ['Mod 247\n' + chelsea_epa_name, 'Mod 238\n' + vh_epa_name, 'Mod 248\n' + vh_epa_name,'Mod 255\n' + vh_epa_name, 'Mod 246\n' + rox_epa_name, 'Mod 256\n' + rox_epa_name, 'Mod 257\n' + rox_epa_name]
+
+# Create a bar chart
+plt.bar(range(len(num_entries)), num_entries, width=0.4, align='center')
+
+# Add labels and title
+plt.xlabel('Data Set')
+plt.ylabel('Number of Entries')
+plt.title('Number of Entries for Each Data Set')
+
+# Add x-axis tick labels
+plt.xticks(range(len(num_entries)), data_labels)
+
+# Show the plot
+plt.show()
+
+# %%
+# Calculate the mean absolute error (MAE) for Google and Quantaq
+mae_quantaq = [mae_247_quantaq, mae_238_quantaq, mae_248_quantaq,  mae_255_quantaq,mae_246_quantaq, mae_256_quantaq]
+mae_google = [mae_247_google, mae_238_google, mae_248_google,  mae_255_google,mae_246_google, mae_256_google]
+
+# Create a bar chart
+plt.bar(range(len(mae_quantaq)), mae_quantaq, width=0.4, align='center', label='Quantaq')
+plt.bar(range(len(mae_google)), mae_google, width=0.4, align='edge', label='Google')
+
+# Add labels and title
+plt.xlabel('Measurement')
+plt.ylabel('Mean Absolute Error (MAE)')
+plt.title('Comparison of MAE between Quantaq and Google')
+
+# Add x-axis tick labels
+tick_labels = ['Mod 247\n' + chelsea_epa_name, 'Mod 238\n' + vh_epa_name, 'Mod 248\n' + vh_epa_name, 'Mod 255\n' + vh_epa_name,'Mod 246\n' + rox_epa_name,  'Mod 256\n' + rox_epa_name]
+plt.xticks(range(len(mae_quantaq)), tick_labels)
+
+# Add legend
+plt.legend()
+
+# Show the plot
+plt.show()
+
+# %%
+# Calculate the root mean squared error (RMSE) for Google and Quantaq
+rmse_quantaq = [rmse_247_quantaq, rmse_238_quantaq, rmse_248_quantaq,  rmse_255_quantaq,rmse_246_quantaq, rmse_256_quantaq]
+rmse_google = [rmse_247_google, rmse_238_google, rmse_248_google,  rmse_255_google,rmse_246_google, rmse_256_google]
+
+# Create a bar chart
+plt.bar(range(len(rmse_quantaq)), rmse_quantaq, width=0.4, align='center', label='Quantaq')
+plt.bar(range(len(rmse_google)), rmse_google, width=0.4, align='edge', label='Google')
+
+# Add labels and title
+plt.xlabel('Measurement')
+plt.ylabel('Root Mean Squared Error (RMSE)')
+plt.title('Comparison of RMSE between Quantaq and Google')
+
+# Add x-axis tick labels
+tick_labels = ['Mod 247\n' + chelsea_epa_name, 'Mod 238\n' + vh_epa_name, 'Mod 248\n' + vh_epa_name, 'Mod 255\n' + vh_epa_name, 'Mod 246\n' + rox_epa_name, 'Mod 256\n' + rox_epa_name]
+plt.xticks(range(len(rmse_quantaq)), tick_labels)
+
+# Add legend
+plt.legend()
+
+# Show the plot
+plt.show()
+# %%
+# Calculate the relative error (RE) for Google and Quantaq
+re_quantaq = [re_247_quantaq, re_238_quantaq, re_248_quantaq,  re_255_quantaq,re_246_quantaq, re_256_quantaq]
+re_google = [re_247_google, re_238_google, re_248_google,  re_255_google,re_246_google, re_256_google]
+
+# Create a bar chart
+plt.bar(range(len(re_quantaq)), re_quantaq, width=0.4, align='center', label='Quantaq')
+plt.bar(range(len(re_google)), re_google, width=0.4, align='edge', label='Google')
+
+# Add labels and title
+plt.xlabel('Measurement')
+plt.ylabel('Relative Error (RE)')
+plt.title('Comparison of RE between Quantaq and Google')
+
+# Add x-axis tick labels
+tick_labels = ['Mod 247\n' + chelsea_epa_name, 'Mod 238\n' + vh_epa_name, 'Mod 248\n' + vh_epa_name,  'Mod 255\n' + vh_epa_name,'Mod 246\n' + rox_epa_name, 'Mod 256\n' + rox_epa_name]
+plt.xticks(range(len(re_quantaq)), tick_labels)
+
+# Add legend
+plt.legend()
+
+# Show the plot
+plt.show()
+
+# %%
--- air_pollution_mapper/data_plotter.py
+++ air_pollution_mapper/data_plotter.py
@@ -53,18 +53,18 @@ def process_mod_data(mod_data,mod_raw_data,mod_name):
     # Calculate dew point using magnus formula
     merged_mod_data['dew_point'] = (243.04 * (np.log(merged_mod_data['rh'] / 100) + ((17.625 * merged_mod_data['temp']) / (243.04 + merged_mod_data['temp']))) / (17.625 - np.log(merged_mod_data['rh'] / 100) - ((17.625 * merged_mod_data['temp']) / (243.04 + merged_mod_data['temp']))))
     dew_point = merged_mod_data['dew_point']
-    print(dew_point)
+    # print(dew_point)
     
     # Filter data based on dew point
     merged_mod_data = merged_mod_data[merged_mod_data['temp'] > merged_mod_data['dew_point']]
-    print(merged_mod_data['dew_point'].head())
+    # print(merged_mod_data['dew_point'].head())
     print(f"Number of rows after dew point filtering: {len(merged_mod_data)}")
 
     merged_mod_data = merged_mod_data[merged_mod_data['flag'] == 0]
     print(f"Number of rows after flag dropping flag entries: {len(merged_mod_data)}")
     
     numeric_cols = merged_mod_data.select_dtypes(include=[np.number]).columns 
-    print(numeric_cols)
+    # print(numeric_cols)
     
     #resample the data to hourly
     mod_data_hourly = merged_mod_data[numeric_cols].resample('h').mean().reset_index()
@@ -88,6 +88,7 @@ def process_google_data(google_data, google_name):
     google_pm25_data = google_data[google_data["code"] == "pm25"].copy()
     google_pm25_data.rename(columns={'value': 'pm25'}, inplace=True)
     google_pm25_data['time'] = pd.to_datetime(google_pm25_data['time'])
+
     google_pm25_data['source'] = f'Google_{google_name}'
 
     return google_pm25_data
@@ -111,7 +112,7 @@ def process_epa_data(google_path):
 
 def plot_data(epa_data,mod_data,google_data, epa_name, mod_name, google_name):
     #plot the data
-    sns.lineplot(x="time", y="pm25", data=epa_data, label=f'{epa_name} EPA')
+    sns.lineplot(x="time", y="pm25", data=epa_data, label=f'{epa_name.capitalize()} EPA')
     sns.lineplot(x="time", y="pm25", data=mod_data, label=f'QuantAQ{mod_name}')
     sns.lineplot(x="time", y="pm25", data=google_data, label=f'Google {google_name}')
 
@@ -122,13 +123,24 @@ def plot_data(epa_data,mod_data,google_data, epa_name, mod_name, google_name):
     plt.show()
 
 
-def merge_data(epa_data,mod_data,epa_name,mod_name):
+def merge_data(epa_data,mod_data,google_pm_data,epa_name,mod_name,google_name):
 
+    # Print the time zones for all three data sets
+    print(f'EPA Time Zone: {epa_data["time"].dt.tz}')
+    print(f'QuantAQ Time Zone: {mod_data["time"].dt.tz}')
+    print(f'Google Time Zone: {google_pm_data["time"].dt.tz}\n')
+
+    print(f'Number of entries in EPA data: {len(epa_data)}')
+    print(f'Number of entries in QuantAQ data: {len(mod_data)}')
+    print(f'Number of entries in Google data: {len(google_pm_data)}')
 
     #merge the data
     merged_data = pd.merge(epa_data, mod_data, on='time', how='inner')
     merged_data.rename(columns={'pm25_x': f'{epa_name} EPA PM2.5', 'pm25_y': f'QuantAQ {mod_name} PM2.5'}, inplace=True)
    
+    # Merge Google data
+    merged_data = pd.merge(merged_data, google_pm_data, on='time', how='inner')
+    merged_data.rename(columns={'pm25': f'Google {google_name} PM2.5'}, inplace=True)
     
     # Drop rows with missing values
     merged_data.dropna(subset=[f'{epa_name} EPA PM2.5', f'QuantAQ {mod_name} PM2.5'], inplace=True)
@@ -136,27 +148,38 @@ def merge_data(epa_data,mod_data,epa_name,mod_name):
     # Set time as the index
     merged_data['time'] = pd.to_datetime(merged_data['time'])
     
+    # Calculate the number of entries in the merged data
+    
+    num_entries = len(merged_data)
+    print(f"Number of matched entries in the merged data: {num_entries}") 
+
     # Plot the merged data
-    sns.scatterplot(x="time", y=f'{epa_name} EPA PM2.5', data=merged_data, label=f'{epa_name} EPA PM2.5')
-    sns.scatterplot(x="time", y=f'QuantAQ {mod_name} PM2.5', data=merged_data, label=f'QuantAQ {mod_name} PM2.5')
+    sns.scatterplot(x="time", y=f'{epa_name} EPA PM2.5', data=merged_data, label=f'{epa_name} EPA PM2.5 ($\\mu$g/m$^3$)')
+    sns.scatterplot(x="time", y=f'QuantAQ {mod_name} PM2.5', data=merged_data, label=f'QuantAQ {mod_name} PM2.5 ($\\mu$g/m$^3$)')
+    sns.scatterplot(x="time", y=f'Google {google_name} PM2.5', data=merged_data, label=f'Google {google_name} PM2.5 ($\\mu$g/m$^3$)')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
-    plt.title(f'Merged Time Series Measurements {mod_name} vs {epa_name}')
+    plt.title(f'Merged Time Series Measurements {mod_name} vs {epa_name.capitalize()}')
     plt.xticks(rotation=90)
     plt.legend()
     plt.show()
     
-    return merged_data
+    return merged_data,num_entries
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_regression_plot(epa_mod_data_merge, epa_name, mod_name):
+# %%
+def plot_regression_plot(epa_mod_data_merge,epa_name, mod_name, google_name):
     # Create a line for the y=x reference
     x_line = np.linspace(0, 16, 100)
     y_line = x_line
+
+    epa_pm25 = epa_mod_data_merge[f'{epa_name} EPA PM2.5']
+    quantaq_pm25 = epa_mod_data_merge[f'QuantAQ {mod_name} PM2.5']
+    google_pm25 = epa_mod_data_merge[f'Google {google_name} PM2.5']
 
     # Create a figure and axis
     fig, ax = plt.subplots()
@@ -165,8 +188,8 @@ def plot_regression_plot(epa_mod_data_merge, epa_name, mod_name):
     ax.plot(x_line, y_line, color='red', label='Line with slope 1')
 
     # Create a scatter plot with a continuous hue
-    scatter = sns.scatterplot(x=epa_mod_data_merge[f'{epa_name} EPA PM2.5'], 
-                              y=epa_mod_data_merge[f'QuantAQ {mod_name} PM2.5'], 
+    scatter = sns.scatterplot(x=epa_pm25, 
+                              y=quantaq_pm25, 
                               hue=epa_mod_data_merge['rh'], 
                               palette='RdYlGn', 
                               edgecolor=None, 
@@ -179,10 +202,92 @@ def plot_regression_plot(epa_mod_data_merge, epa_name, mod_name):
                 line_kws={'linestyle': 'dotted', 'color': 'blue'}, 
                 label='Regression Line', 
                 ax=ax)
+    
+        # Determine the limits
+    max_limit = max(ax.get_xlim()[1], ax.get_ylim()[1])
+    min_limit = min(ax.get_xlim()[0], ax.get_ylim()[0])
+
+    # Set the same limits for both x and y axes
+    ax.set_xlim(min_limit, max_limit)
+    ax.set_ylim(min_limit, max_limit)
 
     # Set labels and title
-    ax.set_xlabel(f'{epa_name} EPA PM2.5')
-    ax.set_ylabel(f'QuantAQ {mod_name} PM2.5')
+    ax.set_xlabel(f'{epa_name.capitalize()} EPA PM2.5 ($\\mu$g/m$^3$)')
+    ax.set_ylabel(f'QuantAQ {mod_name} PM2.5 ($\\mu$g/m$^3$)')
+    ax.set_title('Comparison of PM2.5 Measurements')
+
+    # Rotate x-axis labels if needed
+    plt.xticks(rotation=90)
+
+    # Add a legend to the plot
+    ax.get_legend().remove()
+
+    # Add a color bar
+    norm = plt.Normalize(epa_mod_data_merge['rh'].min(), epa_mod_data_merge['rh'].max())
+    sm = plt.cm.ScalarMappable(cmap='RdYlGn', norm=norm)
+    sm.set_array([])
+    cbar = fig.colorbar(sm, ax=ax, label='Relative Humidity (%)')
+
+    # Display the plot
+    plt.show()
+
+    slope, intercept, r, p, std_err = stats.linregress(epa_mod_data_merge[f'{epa_name} EPA PM2.5'],epa_mod_data_merge[f'QuantAQ {mod_name} PM2.5'])
+    
+    # Round the results to the desired number of decimal places
+    decimal_places = 3  # Change this to the number of decimal places you want
+    slope = round(slope, decimal_places)
+    intercept = round(intercept, decimal_places)
+    r_squared = round(r**2, decimal_places)
+    p_value = round(p, decimal_places)
+    std_err = round(std_err, decimal_places)
+
+    # Print the results
+    print(f'QuantAQ Sensor: {mod_name}') 
+    print(f'EPA Location: {epa_name.capitalize()}')
+    print(f"Slope: {slope}")
+    print(f"Intercept: {intercept}")
+    print(f"R-squared: {r_squared}")
+    if p < 0.001:
+        print(f"P-value: < 0.001")
+    else:
+        print(f"P-value: {p_value}")
+    print(f"Standard error: {std_err}")
+ 
+ #############################################################################
+
+ # Create a figure and axis
+    fig, ax = plt.subplots()
+
+    # Plot the line with a slope of 1
+    ax.plot(x_line, y_line, color='red', label='Line with slope 1')
+
+    # Create a scatter plot with a continuous hue
+    scatter = sns.scatterplot(x=epa_pm25, 
+                              y=google_pm25, 
+                              hue=epa_mod_data_merge['rh'], 
+                              palette='RdYlGn', 
+                              edgecolor=None, 
+                              ax=ax)
+
+    # Add a regression line
+    sns.regplot(x=epa_mod_data_merge[f'{epa_name} EPA PM2.5'], 
+                y=epa_mod_data_merge[f'Google {google_name} PM2.5'], 
+                scatter=False, 
+                line_kws={'linestyle': 'dotted', 'color': 'blue'}, 
+                label='Regression Line', 
+                ax=ax)
+    
+        # Determine the limits
+    max_limit = max(ax.get_xlim()[1], ax.get_ylim()[1])
+    min_limit = min(ax.get_xlim()[0], ax.get_ylim()[0])
+
+    # Set the same limits for both x and y axes
+    ax.set_xlim(min_limit, max_limit)
+    ax.set_ylim(min_limit, max_limit)
+
+    # Set labels and title
+    ax.set_xlabel(f'{epa_name.capitalize()} EPA PM2.5 ($\\mu$g/m$^3$)')
+    ax.set_ylabel(f'Google {google_name} PM2.5 ($\\mu$g/m$^3$)')
     ax.set_title('Comparison of PM2.5 Measurements')
 
     # Rotate x-axis labels if needed
@@ -200,32 +305,88 @@ def plot_regression_plot(epa_mod_data_merge, epa_name, mod_name):
     # Display the plot
     plt.show()
 
-    slope, intercept, r, p, std_err = stats.linregress(epa_mod_data_merge[f'{epa_name} EPA PM2.5'],epa_mod_data_merge[f'QuantAQ {mod_name} PM2.5'])
-    print(f'QuantAQ Sensor: {mod_name} EPA Location: {epa_name} Slope: {slope}, Intercept: {intercept}, R: {r}, P: {p}, Std Err: {std_err}')
+    slope, intercept, r, p, std_err = stats.linregress(epa_mod_data_merge[f'{epa_name} EPA PM2.5'],epa_mod_data_merge[f'Google {google_name} PM2.5'])
+    
+    # Round the results to the desired number of decimal places
+    decimal_places = 3  # Change this to the number of decimal places you want
+    slope = round(slope, decimal_places)
+    intercept = round(intercept, decimal_places)
+    r_squared = round(r**2, decimal_places)
+    p_value = round(p, decimal_places)
+    std_err = round(std_err, decimal_places)
 
+    # Print the results
+    print(f'Google Prediction: {google_name}') 
+    print(f'EPA Location: {epa_name.capitalize()}')
+    print(f"Slope: {slope}")
+    print(f"Intercept: {intercept}")
+    print(f"R-squared: {r_squared}")
+    if p < 0.001:
+        print(f"P-value: < 0.001")
+    else:
+        print(f"P-value: {p_value}")
+    print(f"Standard error: {std_err}")
 
-def calculate_error(merged_data, epa_name, mod_name):
-    error = merged_data[f'QuantAQ {mod_name} PM2.5'] - merged_data[f'{epa_name} EPA PM2.5']
-    error_df = pd.DataFrame({'time': merged_data['time'], 'error': error})
+def calculate_error_metrics(actual, predicted):
+    mae = np.mean(np.abs(actual - predicted))
+    rmse = np.sqrt(np.mean((actual - predicted) ** 2))
+    re = np.mean(np.abs((actual - predicted) / actual))
+    return mae, rmse, re
+
+ # %%
+def calculate_error(merged_data, epa_name, mod_name, google_name):
+    # Calculate the residuals between the QuantAQ and EPA PM2.5 values
+    error_quantaq = merged_data[f'QuantAQ {mod_name} PM2.5'] - merged_data[f'{epa_name} EPA PM2.5']
+    error_df = pd.DataFrame({'time': merged_data['time'], 'Error Quantaq': error_quantaq})
+
+    #calculate residuals between google and epa
+    error_google = merged_data[f'Google {google_name} PM2.5'] - merged_data[f'{epa_name} EPA PM2.5']
+    error_df['Error Google'] = error_google
     
 
+    #plot residuals in histogram
     sns.histplot(error_df, kde=True)
     plt.xlabel('Error')
     plt.ylabel('Frequency')
-    plt.title(f'Distribution of Error {mod_name} vs {epa_name}')
+    plt.title(f'Distribution of Residuals {mod_name} vs {epa_name}')
     plt.show()
 
 
-
-    plt.scatter(error_df['time'], error_df['error'], label=f'{mod_name} Error')
+    #plot residuals over time
+    plt.scatter(error_df['time'], error_df['Error Quantaq'], label=f'{mod_name} Error')
+    plt.scatter(error_df['time'], error_df['Error Google'], label=f'{google_name} Error')
     plt.xlabel('Time')
     plt.ylabel('Error')
     plt.xticks(rotation=90)
-    plt.title(f'Comparison of Error Over Time {mod_name} vs {epa_name}')
+    plt.title(f'Comparison of Residuals Over Time {mod_name} vs {epa_name.capitalize()}')
     plt.legend()
     plt.show()
     
-    return error_df
+    epa_data = merged_data[f'{epa_name} EPA PM2.5']
+    quantaq_data = merged_data[f'QuantAQ {mod_name} PM2.5']
+    google_data = merged_data[f'Google {google_name} PM2.5']
+
+    # Calculate error metrics for QuantAQ data
+    mae_quantaq, rmse_quantaq, re_quantaq = calculate_error_metrics(epa_data, quantaq_data)
+
+     # Print error metrics
+    print('\nQuantAQ Error Metrics')
+    decimal_places = 3  # Change this to the number of decimal places you want
+    print(f"Mean Absolute Error (MAE) QuantAQ: {round(mae_quantaq, decimal_places)}")
+    print(f"Root Mean Square Error (RMSE) QuantAQ: {round(rmse_quantaq,decimal_places)}")
+    print(f"Relative Error (RE) QuantAQ: {round(re_quantaq,decimal_places)}")
+
+    # Calculate error metrics for Google data
+    mae_google, rmse_google, re_google = calculate_error_metrics(epa_data, google_data)
+
+    # Print error metrics
+    print('\nGoogle Error Metrics')
+    print(f"Mean Absolute Error (MAE) Google: {round(mae_google,decimal_places)}")
+    print(f"Root Mean Square Error (RMSE) Google: {round(rmse_google,decimal_places)}")
+    print(f"Relative Error (RE) Google: {round(re_google,decimal_places)}")
+
+    return error_df, mae_quantaq, rmse_quantaq, re_quantaq, mae_google, rmse_google, re_google
+
 
 
 
@@ -246,10 +407,16 @@ google_pm25_247_data = process_google_data(google_247_data, google247_name)
 
 # Plot the data
 plot_data(chelsea_epa_data,mod_pm25_247_data,google_pm25_247_data,chelsea_epa_name,mod247_name,google247_name)
-epa_mod_247_data = merge_data(chelsea_epa_data,mod_pm25_247_data,chelsea_epa_name,mod247_name)
+merged_247_data,merged_247_entries = merge_data(chelsea_epa_data,mod_pm25_247_data,google_pm25_247_data,chelsea_epa_name,mod247_name,google247_name)
+
+# %%
 
 #calculate error
-plot_regression_plot(epa_mod_247_data,chelsea_epa_name,mod247_name)
-error_247 = calculate_error(epa_mod_247_data, chelsea_epa_name, mod247_name)
+plot_regression_plot(merged_247_data,chelsea_epa_name,mod247_name,google247_name)
+
+
+# %%
+error_247, mae_247_quantaq, rmse_247_quantaq, re_247_quantaq, mae_247_google, rmse_247_google, re_247_google = calculate_error(merged_247_data, chelsea_epa_name, mod247_name, google247_name)
+
 
 
